3b1e6ccd16f441842284731124fef1ca46d88c05
s44khin <s44khin@gmail.com>
2022-01-29 22:01:44 +0800

    init text styles

--- app/src/main/kotlin/ru/s44khin/crypto/MainActivity.kt
+++ app/src/main/kotlin/ru/s44khin/crypto/MainActivity.kt
@@ -1,5 +1,17 @@
 package ru.s44khin.crypto
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import by.kirich1409.viewbindingdelegate.viewBinding
+import ru.s44khin.crypto.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding by viewBinding(ActivityMainBinding::bind)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
